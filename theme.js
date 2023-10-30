8a858f5480c67debed4ce1ba6097c8d99497b594
Samuel Ige <samuelige.iyanu@gmail.com>
2023-10-30 01:09:34 +0100

    theme set up

---
+++ theme.js
@@ -0,0 +1,10 @@
+module.exports = {
+    fontFamily: {
+        degular: ["Degular", "sans-serif"],
+    },
+    colors: {},
+    backgroundColor: {},
+    backgroundImage: {},
+    borderColor: {},
+    
+}
